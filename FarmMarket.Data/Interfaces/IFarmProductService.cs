7a4c417fffd1e5bb813b16d7dbecdcc5189c179c
obiora-nwude <122012709+obiora-nwude@users.noreply.github.com>
2023-06-04 00:53:28 +0100

    add farm product service and interface class

---
+++ FarmMarket.Data/Interfaces/IFarmProductService.cs
@@ -0,0 +1,20 @@
+﻿using FarmMarket.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmMarket.Data.Interfaces
+{
+    public interface IFarmProductService
+    {
+        IEnumerable<FarmProduct> GetFarmProducts();
+        FarmProduct GetFarmProduct(Guid productId);
+        IEnumerable<FarmProduct> GetFarmProducts(string productName);
+        Task<FarmProduct> AddFarmProduct(FarmProduct product);
+        bool UpdateFarmProduct(FarmProduct farmProduct);
+        void DeleteFarmProduct(Guid productId);
+        bool Save();
+    }
+}
---
+++ FarmMarket.Service/Services/FarmProductService.cs
@@ -0,0 +1,63 @@
+﻿using FarmMarket.Data;
+using FarmMarket.Data.Interfaces;
+using FarmMarket.Data.Model;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmMarket.Service.Services
+{
+    public class FarmProductService : IFarmProductService
+    {
+        private readonly ApplicationDbContext _context;
+        public FarmProductService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<FarmProduct> AddFarmProduct(FarmProduct product)
+        {
+            var farmProduct = _context.FarmProducts.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public void DeleteFarmProduct(Guid productId)
+        {
+            var farmProduct = _context.FarmProducts.Where(p => p.Id == productId).FirstOrDefault();
+            if (farmProduct != null)
+            {
+                _context.FarmProducts.Remove(farmProduct);
+                _context.SaveChanges();
+            }
+        }
+
+        public FarmProduct GetFarmProduct(Guid productId)
+        {
+            var farmProduct = _context.FarmProducts.Where(p => p.Id == productId).FirstOrDefault();
+            return farmProduct;
+        }
+
+        public IEnumerable<FarmProduct> GetFarmProducts()
+        {
+            var farmProducts = _context.FarmProducts.OrderBy(p => p.Id).ToList();
+            return farmProducts;
+        }
+
+        public bool UpdateFarmProduct(FarmProduct product, Guid productId)
+        {
+            var farmProduct = _context.FarmProducts.Where(p => p.Id == productId).FirstOrDefault();
+            _context.FarmProducts.Update(farmProduct);
+            return true;
+
+        }
+
+        public IEnumerable<FarmProduct> GetFarmProducts(string productName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
